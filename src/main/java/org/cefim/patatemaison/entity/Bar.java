5183399443d6e1e325e5715e5f2d6ef284c95b7e
J-Phiz <jp.save@gmail.com>
2021-07-23 10:04:49 +0200

    Prepare save reservation

--- src/main/java/org/cefim/patatemaison/entity/Bar.java
+++ src/main/java/org/cefim/patatemaison/entity/Bar.java
@@ -1,12 +1,14 @@
 package org.cefim.patatemaison.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Bar {
 
     private Long id;
@@ -30,5 +32,10 @@ public class Bar {
     private String city;
 
     private List<Cocktail> cocktails;
+
+    public Bar (Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
 }
