f70014825c11f7d29f45c2d3c001749e62393dff
Léonie Dusart <leonie.dusart@gmail.comm>
2021-07-23 12:17:54 +0200

    add search by name

--- src/main/java/org/cefim/patatemaison/controller/FindBarController.java
+++ src/main/java/org/cefim/patatemaison/controller/FindBarController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -20,9 +21,13 @@ public class FindBarController {
     @Autowired
     private BarService barService;
 
-
     @GetMapping()
-    public String getEmpty(Model model, @RequestParam(required = false) String search) throws APIException {
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("cocktailForm")
+    public String getCocktailForm(Model model, @RequestParam(required = false) String search) throws APIException {
         model.addAttribute("cocktail", new Cocktail());
         if(search == null)
         {
@@ -41,16 +46,42 @@ public class FindBarController {
         return "findbar";
     }
 
+    @GetMapping("nameForm")
+    public String getBarNameForm(Model model, @RequestParam(required = false) String search) throws APIException {
+        model.addAttribute("bar", new Bar());
+        if(search == null)
+        {
+            model.addAttribute("bars", new ArrayList<>());
+        }
+        else
+        {
+            List<Bar> bars = barService.getBarWithName(search);
+            model.addAttribute("bars", bars);
+            model.addAttribute("previousBarSearch", search);
+            if(bars.size() == 0)
+            {
+                model.addAttribute("message", "Aucun bar correspondant à votre recherche");
+            }
+        }
+        return "findbarbyname";
+    }
+
     @GetMapping("bar/{id}")
-    public String getBarInfo(Model model, @PathVariable("id") int id, @RequestParam(required = false) String search) throws APIException {
+    public String getBarInfo(Model model, @PathVariable("id") int id, @RequestParam Map<String, String> allRequestParams) throws APIException {
         model.addAttribute("bar", barService.getBarInfo(id));
-        if(search != null) {
-            model.addAttribute("previousSearch", search);
+        if(allRequestParams.containsKey("previousBarSearch")) {
+            model.addAttribute("previousBarSearch", allRequestParams.get("previousBarSearch"));
+        }
+        else if(allRequestParams.containsKey("previousCocktailSearch")) {
+            model.addAttribute("previousCocktailSearch", allRequestParams.get("previousCocktailSearch"));
+        }
+        else {
+            model.addAttribute("noPreviousSearch", true);
         }
         return "barinfo";
     }
 
-    @PostMapping(params="find")
+    @PostMapping(path="cocktailForm", params="find")
     public String getBarsWithCocktailName(Model model, @ModelAttribute("cocktail") Cocktail cocktail) throws APIException {
         List<Bar> bars = barService.getBarsWithCocktailName(cocktail.getStrDrink());
         model.addAttribute("bars", bars);
@@ -60,6 +91,18 @@ public class FindBarController {
             model.addAttribute("message", "Aucun bar ne propose le cocktail souhaité");
         }
         return "findbar";
+    }
+
+    @PostMapping(path="nameForm", params="find")
+    public String getBarsWithName(Model model, @ModelAttribute("bar") Bar bar) throws APIException {
+        List<Bar> bars = barService.getBarWithName(bar.getName());
+        model.addAttribute("bars", bars);
+        model.addAttribute("previousBarSearch", bar.getName());
+        if(bars.size() == 0)
+        {
+            model.addAttribute("message", "Aucun bar ne correspond à votre recherche");
+        }
+        return "findbarbyname";
     }
 
 }
--- src/main/java/org/cefim/patatemaison/service/BarService.java
+++ src/main/java/org/cefim/patatemaison/service/BarService.java
@@ -16,7 +16,10 @@ public class BarService {
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Value("${msrecherche.url.findbars}")
-    private String findBarUrl;
+    private String findBarCocktailUrl;
+
+    @Value("${msrecherche.url.findbarsbyname}")
+    private String findBarNameUrl;
 
     @Value("${msrecherche.url.findbarscocktailid}")
     private String findBarCocktailIdUrl;
@@ -28,8 +31,17 @@ public class BarService {
     private String barInfoCocktailsUrl;
 
     public List<Bar> getBarsWithCocktailName(String name) throws APIException {
-        System.out.println(String.format("%s%s", findBarUrl, name));
-        Bar[] barArray = this.restTemplate.getForObject(String.format("%s%s", findBarUrl, name), Bar[].class);
+        Bar[] barArray = this.restTemplate.getForObject(String.format("%s%s", findBarCocktailUrl, name), Bar[].class);
+
+        if (barArray == null) {
+            throw new APIException("Pas de Bar trouvé", HttpStatus.NOT_FOUND);
+        }
+
+        return Arrays.asList(barArray);
+    }
+
+    public List<Bar> getBarWithName(String name) throws APIException {
+        Bar[] barArray = this.restTemplate.getForObject(String.format("%s%s", findBarNameUrl, name), Bar[].class);
 
         if (barArray == null) {
             throw new APIException("Pas de Bar trouvé", HttpStatus.NOT_FOUND);
